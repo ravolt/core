2bd3a991287ab97468248206df60e41b4ddc2a11
Rob Hirschfeld <rob@zehicle.com>
2015-03-04 17:26:28 -0500

    copy default config if missing

--- BDD/linux_compile.sh
+++ BDD/linux_compile.sh
@@ -1,3 +1,9 @@
+# copy over files
+if [[ ! -f default.config ]]; then
+    cp example.config default.config
+    cp dev.sample dev.config
+fi
+
 # Clean-up
 rm -f erl_crash.dump *.beam trace_*.txt
 
